867e8c66ee4abc55d1f447a237828b3d7b73aefa
divyansh <divyanshbansal169@gmail.com>
2025-01-03 20:03:35 +0530

    updated
    updated

--- backend/src/main/java/org/cce/backend/exception/UserAlreadyExistsException.java
+++
@@ -1,9 +0,0 @@
-package org.cce.backend.exception;
-
-import org.springframework.security.core.AuthenticationException;
-
-public class UserAlreadyExistsException extends RuntimeException {
-    public UserAlreadyExistsException(String msg) {
-        super(msg);
-    }
-}
--- backend/src/main/java/org/cce/backend/service/DocAuthorizationService.java
+++
@@ -1,9 +0,0 @@
-package org.cce.backend.service;
-
-import org.cce.backend.entity.Doc;
-
-public interface DocAuthorizationService {
-    boolean canAccess(String docId);
-    boolean canEdit(String username, Doc doc);
-    boolean fullAccess(String username, Doc doc);
-}
